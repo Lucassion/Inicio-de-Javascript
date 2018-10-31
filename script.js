fca53409c004d90686d026640123778e123182ec
Lucassion <lucasinventor@gmail.com>
2018-10-30 22:05:17 -0300

    Inicio Javascript

---
+++ script.js
@@ -0,0 +1,25 @@
+let nome = 'Lucas Kiyota da Costa';
+let idade = 17;
+let vivo = false;
+
+//let saida1 = 'Olá, '+ nome + 'Tudo bem com você?'; O jeito ser é o de baixo
+//saida1 = `Olá, ${nome}! Você tem ${idade} anos!`;
+
+//console.log(prompt('Qual seu nome?'));
+//alert ("Hello world!");
+//console.log ("Passou do alert :)!");
+
+//idade = prompt('Qual sua idade?');
+//vivo = confirm('Você está vivo?');
+//console.log(`Você está vivo? ${vivo}`);
+
+let coxinha = 3.5;
+let entrada = prompt('Quanto dinheiro você tem?');
+let entrada2 = prompt('Quantas coxinhas você quer?');
+let valortotal = coxinha*entrada2;
+let soma = entrada/valortotal;
+let troco = entrada-valortotal;
+
+
+let saida = alert(`Você pode comprar ${soma}`);
+let saida2 = alert(`Seu troco é ${troco}`);
